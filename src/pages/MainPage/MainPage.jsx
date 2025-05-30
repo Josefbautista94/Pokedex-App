ce70aa5690444646f7094c4e4e1a892e1dcc3d0b
Jose <josefbautista94@gmail.com>
2025-05-30 00:55:36 -0400

    Created a component that displays Pokemon Description and Image

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,12 @@
 import './MainPage.css';
 import SearchBar from "../../components/SearchBar/SearchBar";
-
+import PokemonDisplay from '../../components/PokemonDisplay/PokemonDisplay';
 export default function MainPage() {
     return(
 <div className = "mainDiv">
       <h1>Welcome to The Pokédex!</h1>
       <SearchBar  />
-
+      <PokemonDisplay/>
 </div>
 
     )
