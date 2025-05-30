e337a2bfe56cb00b620cdae48de2f7ddc16e6857
Jose <josefbautista94@gmail.com>
2025-05-30 01:13:03 -0400

    In the process of working on fetching pokemon data from the PokeApi

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,26 @@
-import './MainPage.css';
+import { useState } from 'react';
 import SearchBar from "../../components/SearchBar/SearchBar";
 import PokemonDisplay from '../../components/PokemonDisplay/PokemonDisplay';
+import pokeApi from '../../api/pokeApi'; 
+import './MainPage.css';
+
 export default function MainPage() {
+
+const [pokemon,setPokemon] = useState(null);
+
+
+const fetchPokemon = async(name)=>{
+    try{
+const res = await pokeApi.get(`pokemon/${name}`);
+const speciesRes = await pokeApi.get(`pokemon-species/${name}`)
+
+    }
+    catch(error){
+        console.error("😓 There was an error fetching the Pokemon:" , error)
+    }
+};
+
+
     return(
 <div className = "mainDiv">
       <h1>Welcome to The Pokédex!</h1>
