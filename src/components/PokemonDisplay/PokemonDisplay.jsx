c5372fe240baea277c54ac2c578628549bcb5a18
Jose <josefbautista94@gmail.com>
2025-05-30 02:09:55 -0400

    feat: align description and image, add responsive stacking for mobile view

--- src/components/PokemonDisplay/PokemonDisplay.jsx
+++ src/components/PokemonDisplay/PokemonDisplay.jsx
@@ -5,11 +5,23 @@ export default function PokemonDisplay({ pokemon }) {
     return null;
   }
 
+  function capitalizeFirstLetter(name) {
+    if (!name) return "";
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
   return (
     <div className="infoDiv">
       <div className="descriptionDiv">{pokemon.flavorText}</div>
-      <div className="pokemonImageDiv">
-        <img className="pokeImage" src={pokemon.image} alt={pokemon.name || 'Pokemon'} />
+      <div className="imageContainer">
+        <h1 className="pokeName">{capitalizeFirstLetter(pokemon.name)}</h1>
+        <div className="pokemonImageDiv">
+          <img
+            className="pokeImage"
+            src={pokemon.image}
+            alt={pokemon.name || "Pokemon"}
+          />
+        </div>
       </div>
     </div>
   );
